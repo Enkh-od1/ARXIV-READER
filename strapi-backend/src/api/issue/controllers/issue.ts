4894dfad44d026b365913f6071370ae09ccf3513
unknown <enkh-od@naog.gov.mn>
2025-12-19 17:10:54 +0800

    Remove strapi-backend folder from Next.js project

--- strapi-backend/src/api/issue/controllers/issue.ts
+++
@@ -1,7 +0,0 @@
-/**
- * issue controller
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreController('api::issue.issue');
--- strapi-backend/src/api/issue/routes/issue.ts
+++
@@ -1,7 +0,0 @@
-/**
- * issue router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::issue.issue');
